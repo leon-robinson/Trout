8f8b68209047660a07e2fd16340753d9fa83b6ed
leon-robinson <leonrobinson.dev@gmail.com>
2022-07-17 20:02:30 +0100

    memset and memcpy

--- kernel/include/string.h
+++ kernel/include/string.h
@@ -4,5 +4,7 @@
 #include <types.h>
 
 size_t strlen(const char *str);
+void *memset(void *str, int c, size_t n);
+void *memcpy(void *dest, const void * src, size_t n);
 
 #endif
--- kernel/src/libc/string.c
+++ kernel/src/libc/string.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <types.h>
 
 size_t strlen(const char *str)
 {
@@ -6,4 +7,59 @@ size_t strlen(const char *str)
 	while (*str++)
 		len++;
 	return len;
+}
+
+void *memset(void *str, int c, size_t n)
+{
+
+	void *d = str;
+
+#define DO_SET(T, sz, v) \
+		do { \
+			size_t x = (size_t)sz; \
+			while (x >= sizeof(T)) { \
+				*(T*)d = (T)v; \
+				x -= sizeof(T); \
+				d = (void *)((char *)(d) + sizeof(T)); \
+				n -= sizeof(T);  \
+			} \
+		} while(0)
+
+	if (n >= 4 && ((uintptr_t)str & 3))
+		DO_SET(u8, n & 3, c);
+
+	DO_SET(u32, n, ((u32)c << 24 | (u32)c << 16 | (u32)c << 8 | (u32)c));
+
+	DO_SET(u8, n, c);
+
+#undef DO_SET
+	return str;
+}
+
+void *memcpy(void *dest, const void * src, size_t n)
+{
+#define DO_COPY(T, sz) \
+		do { \
+			size_t x = (size_t)sz; \
+			while (x >= sizeof(T)) { \
+				*(T*)d = *(T*)s; \
+				x -= sizeof(T); \
+				s += sizeof(T); \
+				d += sizeof(T); \
+				n -= sizeof(T);  \
+			} \
+		} while(0)
+
+	char *d = (char *)dest;
+	const char *s = (const char *)src;
+
+	if (n >= 4 && ((uintptr_t)dest & 3))
+		DO_COPY(u8, n & 3);
+
+	DO_COPY(u32, n);
+
+	DO_COPY(u8, n);
+
+#undef DO_COPY
+	return dest;
 }
